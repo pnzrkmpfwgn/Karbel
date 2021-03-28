d7a9179a0cf155426d4b362fea0388419cfb7a9e
Ayberk Yigit <kurtknispel9@gmail.com>
2021-03-28 14:47:48 +0300

    added new services, tweaked seo

--- src/data/ServiceData.js
+++ src/data/ServiceData.js
@@ -6,25 +6,44 @@ export const ServiceData=[
         image4:'./assets/services/11_ç.jpeg',
         image5:'./assets/services/12_ç.jpeg',
         title:'Çelik Konstriksiyon',
-        key:1
+        key:'Çelik Konstriksiyon'
     },
     {
-        image1:'./assets/services/1_s.jpg',
-        image2:'./assets/services/2_s.jpg',
-        image3:'./assets/services/3_s.jpg',
-        image4:'./assets/services/4_s.jpg',
-        image5:'./assets/services/5_s.jpg',
-        title:'Paslanmaz Silo İmalatı',
-        key:2
+        image1:'./assets/services/3_s.jpeg',
+        image2:'./assets/services/2_s.jpeg',
+        image3:'./assets/services/1_s.jpeg',
+        image4:'./assets/services/2_s.jpeg',
+        image5:'./assets/services/3_s.jpeg',
+        title:'Mimari Tasarım',
+        key:'Mimari Tasarım'
     },
     {
-        image1:'./assets/services/12_k.jpg',
-        image2:'./assets/services/13_k.jpg',
-        image3:'./assets/services/14_k.jpg',
-        image4:'./assets/services/15_k.jpg',
-        image5:'./assets/services/16_k.jpg',
-        title:'Çelik Tank & Yağ Kazanları',
-        key:3
+        image1:'./assets/services/1_d.jpeg',
+        image2:'./assets/services/2_d.jpeg',
+        image3:'./assets/services/3_d.jpeg',
+        image4:'./assets/services/4_d.jpeg',
+        image5:'./assets/services/5_d.jpeg',
+        title:'İstediğiniz Ölüçlerde ve Özelliklerde Anahtar Teslim, Depo Dükkan Kapalı Alan İmalatları',
+        key:'Depo Dükkan Kapalı Alan İmalatı'
+    },
+    {
+        image1:'./assets/services/1_su.jpeg',
+        image2:'./assets/services/2_su.jpeg',
+        image3:'./assets/services/3_su.jpeg',
+        image4:'./assets/services/4_su.jpeg',
+        image5:'./assets/services/5_su.jpeg',
+        title:'Sundurma Çatı İmalatı',
+        key:'Sundurma Çatı İmalatı'
+    },
+    {
+        image1:'./assets/services/1_tra.jpeg',
+        image2:'./assets/services/2_tra.jpeg',
+        image3:'./assets/services/3_tra.jpeg',
+        image4:'./assets/services/4_tra.jpeg',
+        image5:'./assets/services/5_tra.jpeg',
+        title:'Çelik Çatı, Trapez Saç, Çatı Panel, Çatı İmalatlarımız',
+        key:'Çelik Çatı Trapez Saç Çatı Panel Çatı İmalatlarımız'
+        
     },
     {
         image1:'./assets/services/15_p.jpg',
@@ -33,7 +52,7 @@ export const ServiceData=[
         image4:'./assets/services/18_p.jpg',
         image5:'./assets/services/19_p.jpg',
         title:'Makina Platform İmalatı',
-        key:4
+        key:'Makina Platform İmalatı'
     },
     {
         image1:'./assets/services/1_B.jpg',
@@ -42,7 +61,7 @@ export const ServiceData=[
         image4:'./assets/services/4_B.jpg',
         image5:'./assets/services/5_B.jpg',
         title:'İnşaat - Fabrika & Batar Kat İmalatı',
-        key:5
+        key:'İnşaat - Fabrika & Batar Kat İmalatı'
     },
     {
         image1:'./assets/services/1_kon.jpg',
@@ -51,7 +70,7 @@ export const ServiceData=[
         image4:'./assets/services/4_kon.jpg',
         image5:'./assets/services/5_kon.jpg',
         title:'Konveyör Bant Sistemleri',
-        key:6
+        key:'Konveyör Bant Sistemleri'
     },
     {
         image1:'./assets/services/1_m.jpg',
@@ -60,7 +79,7 @@ export const ServiceData=[
         image4:'./assets/services/4_m.jpg',
         image5:'./assets/services/5_m.jpg',
         title:'Makina İmalatı & Torna Freze İşleri',
-        key:7
+        key:'Makina İmalatı & Torna Freze İşleri'
     },
     {
         image1:'./assets/services/1_h.jpg',
@@ -69,7 +88,6 @@ export const ServiceData=[
         image4:'./assets/services/4_h.jpg',
         image5:'./assets/services/5_h.jpg',
         title:'Homojenizatör - Helezon - Sarsak Elek & Elavatör',
-        text:'',
-        key:8
+        key:'Homojenizatör - Helezon - Sarsak Elek & Elavatör'
     }
 ]
