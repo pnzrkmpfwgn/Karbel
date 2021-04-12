bc36ddeb597807b649b1eac95fa6e92db022610f
Ayberk Yigit <kurtknispel9@gmail.com>
2021-04-12 20:52:21 +0300

    fixed non-www issue, added reference

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ const App=()=>{
         delay:2000
     })
     useEffect(()=>{
-        if(window.location.href.startsWith('www.')){
+        if(window.location.href.startsWith('https://www.')){
             window.location.href="https://karbelcelik.com/"
             console.log("executedf")
         }else{
