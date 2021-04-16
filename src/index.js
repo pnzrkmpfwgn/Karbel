afae57ba88da23e0bfb217d22c23a27f6a020b48
Ayberk Yigit <kurtknispel9@gmail.com>
2021-04-16 23:04:52 +0300

    added robots.txt, tweaked SEO using helmet, fixed visual issues

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {HelmetProvider,Helmet} from 'react-helmet-async';
 import App from './App';
 
+const app = (
+    <HelmetProvider>
+        <App>
+            <Helmet>
+                <meta
+                    name="description"
+                    content="Karbel Çelik şirketi, İzmir merkezli çelik konstrüksiyon şirketidir. Sandviç panel, çelik çatı, batar kat imalatı, mimari tasarım, makine imalatı ve birçok hizmeti bulunmaktadır."
+                />
+                <meta name="keywords" content="Karbel, Karbel Çelik, Mimar, Mimari, Tasarım, Mimari Tasarım, Karbel Mimari Tasarım, İzmir Mimari, İzmir Mimari Tasarım, İzmir Karbel Mimari Tasarım, Sundurma, Sundurma Çatı, Trapez, Trapez Saç, Çatı Panel,, Karbel Çelik konstrüksiyon, Çelik, Çatı, Çelik çatı, İzmir, İzmir çelik çatı, İzmir çelik, İzmir Karbel Çelik,Sandviç panel, Sandviç panel çatı,İzmir sandviç panel,İzmir sandviç panel çatı, çatı, Konveyör,İzmir konveyör, Helezon,İzmir helezon, Homojenizatör,İzmir homojenizatör, Paslanmaz çelik,İzmir paslanmaz çelik, Depo,İzmir depo, Çelik depo,İzmir çelik depo, İnşaat batar kat,İzmir inşaat batar kat, Fabrika batar kat,İzmir fabrika batar kat, Makina,Makina platform,İzmir makina platform, İmalat,İzmir imalat, Makina imalat,İzmir makina imalat, Karbel makina, Karbel imalat" />
+                <meta name="author" content="Ülkü Ayberk Yiğit" />
+                <title> Karbel Çelik </title>
+            </Helmet>
+        </App>
+    </HelmetProvider>
+)
 
-ReactDOM.render(  <App />,document.getElementById('root')
+ReactDOM.render(  app,document.getElementById('root')
 );
 
 
